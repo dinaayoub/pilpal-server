b651b93354e92f05a2cec5466c2a516c7c7479fc
Dina Ayoub <dinaayoub@hotmail.com>
2021-01-28 10:32:52 -0800

    added harry potter trivia schema

---
+++ src/models/trivia.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+// 1. make a schema
+const triviaSchema = mongoose.Schema({
+  category: { type: String },
+  answer: { type: String, required: true },
+  question: { type: String, required: true }
+});
+
+// 2. export this schema as a model
+const triviaModel = mongoose.model('potter', triviaSchema);
+
+module.exports = triviaModel;
